baaf893f61dd14590b79a165ffd61362caf368c2
ledsouza <leandro.souza.159@gmail.com>
2024-06-03 12:41:37 -0300

    configurado o admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',)
