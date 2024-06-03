57d0babee97f6163af4043c9f39ce45d1446f4dc
ledsouza <leandro.souza.159@gmail.com>
2024-06-03 12:10:08 -0300

    ajuste no model Curso

--- escola/models.py
+++ escola/models.py
@@ -17,8 +17,10 @@ class Curso(models.Model):
         ('I', 'Intermediário'),
         ('A', 'Avançado')
     )
-
     codigo_curso = models.CharField(max_length=10)
     descricao = models.CharField(max_length=100)
     nivel = models.CharField(max_length=1, choices=NIVEL,
                              blank=False, null=False, default='B')
+
+    def __str__(self):
+        return self.descricao
