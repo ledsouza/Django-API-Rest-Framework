e189e043161e58d57dc0394f5bc246c71415179c
ledsouza <leandro.souza.159@gmail.com>
2024-06-03 15:45:20 -0300

    criando as rotas e view para matriculas

--- escola/urls.py
+++ escola/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from escola.views import AlunoViewSet, CursoViewSet
+from escola.views import AlunoViewSet, CursoViewSet, MatriculaViewSet
 
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunoViewSet, basename='Alunos')
 router.register('cursos', CursoViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('', include(router.urls))
--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializer import AlunoSerializer, CursoSerializer
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
 
 
 class AlunoViewSet(viewsets.ModelViewSet):
@@ -13,3 +13,9 @@ class CursoViewSet(viewsets.ModelViewSet):
     """Exibe todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Exibe todas as matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
